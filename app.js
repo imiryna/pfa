3110f8636cf2d0877b3365a111bb45e1db354f6a
Iryna Burlaka <i.am.blinova@gmail.com>
2025-08-15 20:17:44 +0200

    set up dotenv, morgan

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 const app = express();
 
+dotenv.config({
+  path: "./.env",
+});
+
+app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Наше проміжне ПЗ");
+  next();
+});
 
 app.post("/users", (req, res) => {
   try {
@@ -24,6 +36,10 @@ app.post("/users", (req, res) => {
   } catch (error) {
     console.log(error);
   }
+});
+
+app.get("/", (req, res) => {
+  console.log(req.query);
 });
 
 module.exports = app; // export instance app
