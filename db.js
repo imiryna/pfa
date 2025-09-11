d74ec07d42f4ea190b09aff34b4259eab94cc1a1
Iryna Burlaka <i.am.blinova@gmail.com>
2025-09-11 12:25:19 +0200

    setup the database connection

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
+const { readFileSync } = require("fs");
 const { Client } = require("pg");
 
 require("dotenv").config();
 
+// Read the SQL file
+const sql = readFileSync("schema.sql", "utf8");
+
 const client = new Client();
 
-async function runQuery(queryString) {
+async function runQuery() {
   try {
     await client.connect();
-    const result = await client.query(queryString);
+    const result = await client.query(sql);
     return result;
   } catch (error) {
     throw error;
